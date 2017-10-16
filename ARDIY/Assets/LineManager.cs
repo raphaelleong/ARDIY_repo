2717a63153980ebf543619e33807a592f4dc2182
Amanda Koh <ysk115@ic.ac.uk>
2017-10-16 18:08:32 +0100

    [Amanda+Phuong] AR Camera Testing

---
+++ ARDIY/Assets/LineManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour {
+
+	public Line[] lines = new Line[5]; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
