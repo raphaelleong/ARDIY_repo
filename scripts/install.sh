047bdc5f6931d630f6dad6e55b724ab72448ee52
ryl15 <ryl15@ic.ac.uk>
2017-10-16 14:51:21 +0100

    fix to install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,28 +7,25 @@ HASH=46dda1414e51
 VERSION=2017.2.0f3
 
 download() {
-    file=$1
-      url="$BASE_URL/$HASH/$package"
+  file=$1
+  url="$BASE_URL/$HASH/$package"
 
-        echo "Downloading from $url: "
-          curl -o `basename "$package"` "$url"
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
 }
 
 install() {
-    package=$1
-      download "$package"
+  package=$1
+  download "$package"
 
-        echo "Installing "`basename "$package"`
-          sudo installer -dumplog -package `basename "$package"` -target /
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
 }
 
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
-install
-"MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-install
-"MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-install
-"MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
