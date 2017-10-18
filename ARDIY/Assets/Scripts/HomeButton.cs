77858fa26499bffad2c71b6c8b9b898582a99d8d
ryl15 <ryl15@ic.ac.uk>
2017-10-18 15:45:33 +0100

    created prefab for home button. will remove from prefabs for instructions and colour picker

---
+++ ARDIY/Assets/Scripts/HomeButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Home_onClick()
+    {
+        Debug.Log("Home pressed");
+        //go to the home screen
+    }
+}
