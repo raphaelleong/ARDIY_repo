6489b04c5ca5823f87ce089cb01b559353cf9f28
ryl15 <ryl15@ic.ac.uk>
2017-10-18 18:03:40 +0100

    redefined Autobuilder.cs

--- ARDIY/Assets/Editor/Autobuilder.cs
+++ ARDIY/Assets/Editor/Autobuilder.cs
@@ -108,9 +108,9 @@ GetProjectName() + ".wdgt",BuildTarget.DashboardWidget,BuildOptions.None);
   [MenuItem("File/AutoBuilder/iOS")]
   static void PerformiOSBuild ()
   {
-    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iPhone);
+    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
     BuildPipeline.BuildPlayer(GetScenePaths(),
-"Builds/iOS",BuildTarget.iPhone,BuildOptions.None);
+"Builds/iOS",BuildTarget.iOS,BuildOptions.None);
   }
   [MenuItem("File/AutoBuilder/Android")]
   static void PerformAndroidBuild ()
