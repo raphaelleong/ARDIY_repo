9000fcd8bb9720581ea145360b9af5e6e67be88a
Raphael Leong <raphael.leong15@imperial.ac.uk>
2017-10-18 14:39:50 +0100

    Autobuilder.cs added from unity wiki

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,29 +2,22 @@
 
 project="ARDIY"
 
-echo "Attempting to build $project for OS X"
+echo "Attempting to build $project"
+
 /Applications/Unity/Unity.app/Contents/MacOS/Unity
   -batchmode
   -nographics
   -silent-crashes
   -logFile $(pwd)/unity.log
   -projectPath $(pwd)
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app"
+  -executeMethod Autobuilder.PerformiOSBuild
   -quit
-
-#echo "Attempting to build $project for iOS"
-#/Applications/Unity/Unity.app/Contents/
-#  -batchmode
-#  -nographics
-#  -silent-crashes
-#  -logFile $(pwd)/unity.log
-#  -projectPath $(pwd)
-#  -build
-#  -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
 echo 'Attempting to compress builds'
+ls 
+
 #zip -r $(pwd)/Build/ios.zip $(pwd)/Build/ios
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+#zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
