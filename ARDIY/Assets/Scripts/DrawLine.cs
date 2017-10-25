6620c29b4ca5ed7881c4f11b17f752c035addf96
jfm15 <jfm15@imperial.ac.uk>
2017-10-25 12:57:44 +0100

    No gap between walls [jfm, jas, ba]

--- ARDIY/Assets/Scripts/DrawLine.cs
+++ ARDIY/Assets/Scripts/DrawLine.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
+using UnityEngine.UI;
 
 public class DrawLine : MonoBehaviour {
   // determines the previous coordinate that was saved
   Vector3? lastCoordinate;
+  Vector3 currentCoordinate;
   // The first coordinate that was saved
   Vector3 origin;
 
   public GameObject wallPrefab;
+  public Text debugText;
 
   /*
   Find the touch point on the screen and draw a wall between two consecutive points
@@ -68,7 +71,7 @@ public class DrawLine : MonoBehaviour {
         }
 
         if (lastCoordinate != null) {
-          drawWall(currentCoordinate);
+			drawWall(lastCoordinate.Value, currentCoordinate);
         } else {
           origin = currentCoordinate;
           anchorPosition();
@@ -83,21 +86,31 @@ public class DrawLine : MonoBehaviour {
   }
 
   /* Draw a wall between current point and the last point by specifying a mesh with 4 vertices */
-  void drawWall(Vector3 currentCoordinate) {
+	void drawWall(Vector3 point1, Vector3 point2) {
 	GameObject wall = Instantiate (wallPrefab);
 	
 	MeshFilter meshFilter = wall.GetComponent (typeof(MeshFilter)) as MeshFilter;
 	Mesh wallMesh = meshFilter.mesh;
-		wallMesh.vertices = new Vector3[] {lastCoordinate.Value, currentCoordinate,
-      new Vector3(currentCoordinate.x, currentCoordinate.y + 10, currentCoordinate.z),
-      new Vector3(lastCoordinate.Value.x, lastCoordinate.Value.y + 10, lastCoordinate.Value.z)};
-    }
+		wallMesh.vertices = new Vector3[] {
+            point1, 
+            point2,
+			new Vector3(point2.x, point2.y + 1, point2.z),
+			new Vector3(point1.x, point1.y + 1, point1.z)};
+    //point1, point2, point2.xyz, point1.xyz
 
-    /* Anchor the origin to the camera */
-    void anchorPosition() {
-      UnityARUserAnchorData anchor = UnityARUserAnchorData.UnityARUserAnchorDataFromGameObject(Camera.main.gameObject);
+    wallMesh.triangles = new int[] { 0, 1, 2, 0, 2, 3 };
 
-      UnityARSessionNativeInterface.GetARSessionNativeInterface ().AddUserAnchor(anchor);
-
+    debugText.text = "";
+    foreach (int vec in wallMesh.triangles) {
+      debugText.text += vec.ToString () + " - ";
     }
   }
+
+	/* Anchor the origin to the camera */
+	void anchorPosition() {
+	  UnityARUserAnchorData anchor = UnityARUserAnchorData.UnityARUserAnchorDataFromGameObject(Camera.main.gameObject);
+
+	  UnityARSessionNativeInterface.GetARSessionNativeInterface ().AddUserAnchor(anchor);
+
+	}
+}
