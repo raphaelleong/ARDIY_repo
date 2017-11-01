fe86f418ea462d6e235e4babbecab3aa734d78d3
Amanda Koh <ysk115@ic.ac.uk>
2017-11-01 17:31:19 +0000

    added UI ignore code to DrawLine

--- ARDIY/Assets/Scripts/DrawLine.cs
+++ ARDIY/Assets/Scripts/DrawLine.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.iOS;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class DrawLine : MonoBehaviour
 {
@@ -30,9 +31,12 @@ public class DrawLine : MonoBehaviour
 	*/
 	void Update ()
 	{
-		if (Input.touchCount > 0) {
-			var touch = Input.GetTouch (0);
-			if (touch.phase == TouchPhase.Began) {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			// Check if finger is over a UI element
+			if (!EventSystem.current.IsPointerOverGameObject (Input.GetTouch (0).fingerId)) {
+				
+				var touch = Input.GetTouch (0);
 				var screenPosition = Camera.main.ScreenToViewportPoint (new Vector3(Screen.width/2, Screen.height/2, Camera.main.nearClipPlane));
 				ARPoint point = new ARPoint {
 					x = screenPosition.x,
