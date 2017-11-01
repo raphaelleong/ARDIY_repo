c2950850d831d1cdb05df74dfe4bb74289c664cb
Raphael Leong <raphael.leong15@imperial.ac.uk>
2017-11-01 11:06:33 +0000

    attempting to observe crash output

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,6 @@ echo "Attempting to build $project"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity
   -batchmode
   -nographics
-  -silent-crashes
   -logFile $(pwd)/unity.log
   -projectPath $(pwd)
   -executeMethod Autobuilder.PerformiOSBuild
