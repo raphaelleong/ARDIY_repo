09454f0b2a1e23ecd7a4534fb071b900b48911d1
jfm15 <jfm15@imperial.ac.uk>
2017-11-08 16:39:45 +0000

    Add Script which cutouts rectangles from a quad mesh

---
+++ ARDIY/Assets/Scripts/CutoutMesh.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutoutMesh : MonoBehaviour {
+
+  Mesh mesh;
+
+	// Use this for initialization
+	void Start () {
+    mesh = GetComponent<MeshFilter> ().mesh;
+	}
+	
+  public void Cutout (Vector3[] cutoutVertices) {
+    /* to simulate what would happen in the real program */
+    Vector3[] temp_vertices = mesh.vertices;
+    Vector3 temp = temp_vertices [1];
+    temp_vertices [1] = temp_vertices [2];
+    temp_vertices [2] = temp;
+    mesh.vertices = temp_vertices;
+    /*    */
+    Vector3[] vertices = mesh.vertices;
+    int closestPointIdx = getClosestPointIdx (cutoutVertices, vertices [0]);
+    Vector3[] newVertices = new Vector3[8];
+    int[] newTriangles = new int[8 * 3];
+    for (int i = 0; i < 4; i++) {
+      newVertices [i] = vertices [i];
+    }
+    for (int i = 0; i < 4; i++) {
+      newVertices[4 + i] = transform.InverseTransformPoint(cutoutVertices[(closestPointIdx + i) % 4]);
+      //if (i == 0) {
+        newTriangles [i * 6] = i;
+        newTriangles [i * 6 + 2] = (i + 1) % 4;
+        newTriangles [i * 6 + 1] = 4 + ((closestPointIdx + i) % 4);
+
+        newTriangles [i * 6 + 3] = i;
+        newTriangles [i * 6 + 4] = 4 + ((closestPointIdx + i - 1) % 4);
+        newTriangles [i * 6 + 5] = 4 + ((closestPointIdx + i) % 4);
+      //}
+    }
+    mesh.vertices = newVertices;
+    mesh.triangles = newTriangles;
+
+    mesh.RecalculateNormals();
+  }
+
+  void CreateCube (Vector3 pos, Color col) {
+    GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+    cube.transform.position = pos;
+    cube.transform.localScale = Vector3.one * 0.05f;
+    cube.GetComponent<MeshRenderer> ().material.color = col;
+  }
+
+  // assumes points.length > 0
+  int getClosestPointIdx (Vector3[] points, Vector3 target) {
+    int closestPointIdx = 0;
+    float leastDis = Mathf.Infinity;
+    for (int i = 1; i < points.Length; i++) {
+      float dis = Vector3.Distance (points [i], target);
+      if (dis < leastDis) {
+        closestPointIdx = i;
+        leastDis = dis;
+      }
+    }
+    return closestPointIdx;
+  }
+}
--- ARDIY/Assets/Scripts/SelectCutoutArea.cs
+++ ARDIY/Assets/Scripts/SelectCutoutArea.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CutoutWall : MonoBehaviour {
+public class SelectCutoutArea : MonoBehaviour {
 
   LineRenderer lineRenderer;
   Mesh mesh; 
 
   bool firstCornerPlaced = false;
   Vector3 firstCorner;
+  bool secondCornerPlaced = false;
   Vector3 secondCorner;
+
+  Vector3 otherCornerA;
+  Vector3 otherCornerB;
+
+  GameObject cube;
+
+  CutoutMesh cutoutMesh;
 
 	// Use this for initialization
 	void Start () {
     lineRenderer = transform.GetChild (0).GetComponent<LineRenderer> ();
     transform.GetChild (0).rotation = Quaternion.Euler (Vector3.zero);
     mesh = GetComponent<MeshFilter> ().mesh;
+    cutoutMesh = GetComponent<CutoutMesh> ();
 	}
 	
 	// Update is called once per frame
@@ -29,20 +38,31 @@ public class CutoutWall : MonoBehaviour {
         if (!firstCornerPlaced) {
           firstCorner = hitPoint;
           firstCornerPlaced = true;
-          GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+          cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
           cube.transform.position = hitPoint;
           cube.transform.localScale = Vector3.one * 0.05f;
         } else {
           secondCorner = hitPoint;
 
           // get other corners
-          Vector3 otherCornerA = new Vector3 (firstCorner.x, secondCorner.y, firstCorner.z);
-          Vector3 otherCornerB = new Vector3 (secondCorner.x, firstCorner.y, secondCorner.z);
+          otherCornerA = new Vector3 (firstCorner.x, secondCorner.y, firstCorner.z);
+          otherCornerB = new Vector3 (secondCorner.x, firstCorner.y, secondCorner.z);
 
           lineRenderer.positionCount = 4;
           lineRenderer.SetPositions(new Vector3[4]{firstCorner, otherCornerA, secondCorner, otherCornerB});
           lineRenderer.loop = true;
+          secondCornerPlaced = true;
         }
+      }
+    }
+
+    if (Input.GetKeyDown (KeyCode.Space)) {
+      if (secondCornerPlaced) {
+        cutoutMesh.Cutout (new Vector3[4]{ firstCorner, otherCornerA, secondCorner, otherCornerB });
+        lineRenderer.enabled = false;
+        Destroy (cube);
+      } else {
+        Debug.Log ("points not selected");
       }
     }
   }
