9a384d79bdc7f8e16d7a693efa5e42024332fe27
ryl15 <ryl15@ic.ac.uk>
2017-11-29 15:23:42 +0000

    script init speech in unity project

--- ARDIY/Assets/Scripts/TextToSpeech.cs
+++ ARDIY/Assets/Scripts/TextToSpeech.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TextToSpeech : MonoBehaviour {
-    
-	// Use this for initialization
+    /* Plugin used from github.com/eralpkaraduman */
+    public TTSPlugin tts;
+
+    private string utterance = "hello world";
+
+    // Use this for initialization
 	void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    public void setUtterance(string text) {
+        utterance = text;
+    }
 
+    public string getUtterance() {
+        return utterance;
+    }
+
+    public void beginSpeechSynthesize() {
+        tts.Begin(utterance);
+    }
 }
